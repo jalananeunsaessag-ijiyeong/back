44e2bedf6f8212a36a4b5061d06e2ac7bdb5cbc6
Heuisub <gusgmltjq@naver.com>
2024-08-02 00:02:17 +0900

    Update server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`서버가 *:${PORT} 포트에서 실행 중입니다.`);
 });
